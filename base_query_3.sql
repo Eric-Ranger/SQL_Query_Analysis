dd25241e8af399ffef618d4faf2f462c3671370d
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2021-08-01 16:36:59 -0700

    Update base_query_3.sql

--- base_query_3.sql
+++ base_query_3.sql
@@ -1,9 +1,11 @@
 Select j.job_id, j.Location_id, Location_name, t.time_year, t.time_month,
-	Sum(cost_labor) As Sum_labor_cost, Sum(cost_material) As Sum_material_cost, Sum(cost_overhead) As Sum_overhead_cost,
+	Sum(cost_labor) As Sum_labor_cost, 
+	Sum(cost_material) As Sum_material_cost, 
+	Sum(cost_overhead) As Sum_overhead_cost,
 	Sum(machine_hours)* Sum(rate_per_hour) As Sum_machine_cost, 
-	Sum(cost_labor)+Sum(cost_material)+Sum(cost_overhead)+(Sum(machine_hours)*Sum(rate_per_hour)) As Sum_total_cost, 
+	Sum(cost_labor + cost_material + cost_overhead) + Sum(machine_hours * rate_per_hour) As Sum_total_cost, 
 	Sum(quantity_produced) As Sum_qty_produced, 
-	(Sum(cost_labor)+Sum(cost_material)+Sum(cost_overhead)+(Sum(machine_hours)*Sum(rate_per_hour)))/Sum(quantity_produced) As Unit_cost
+	(Sum(cost_labor + cost_material + cost_overhead) + Sum(machine_hours * rate_per_hour))/Sum(quantity_produced) As Unit_cost
 From w_job_f As j, w_location_d As l, w_time_d As t, w_sub_job_f As su, w_machine_type_d As m
 Where j.location_id = l.location_id
 	And j.job_id = su.job_id
