00b4e9aeceb750805eba93640282176824eac1f8
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2021-08-01 21:45:40 -0700

    Update analytical_query_5.sql

--- analytical_query_5.sql
+++ analytical_query_5.sql
@@ -1,7 +1,7 @@
 Select irs.job_id, irs.location_name, irs.time_year,
-	(Sum(sum_invoice_amt) - Sum(sum_total_cost))/Sum(sum_invoice_amt) As profit_margin,
+	Sum(sum_invoice_amt - sum_total_cost)/Sum(sum_invoice_amt) As profit_margin,
 	Percent_RANK()OVER (
-		ORDER BY ((Sum(sum_invoice_amt) - Sum(sum_total_cost))/(Sum(sum_invoice_amt)))DESC) AS profit_Rank
+		ORDER BY (Sum(sum_invoice_amt - sum_total_cost)/Sum(sum_invoice_amt))DESC) AS profit_Rank
 From invoice_revenue_summary As irs, location_subjob_cost_summary As scs
 Where irs.job_id = scs.job_id
 Group By irs.job_id, irs.location_name, irs.time_year
