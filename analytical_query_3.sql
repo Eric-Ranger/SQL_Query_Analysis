ce26957af3f4648902321754cce226b45a33eccb
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2021-08-01 21:08:47 -0700

    Update analytical_query_3.sql

--- analytical_query_3.sql
+++ analytical_query_3.sql
@@ -1,5 +1,5 @@
 Select irs.location_name, irs.time_year,
-	Sum(sum_invoice_amt) - Sum(sum_total_cost) As profit,
+	Sum(sum_invoice_amt - sum_total_cost)::numeric(10,2) As profit,
 	RANK()OVER (PARTITION BY irs.time_year 
 		ORDER BY (Sum(sum_invoice_amt) - Sum(sum_total_cost))DESC) AS profit_Rank
 From invoice_revenue_summary As irs, location_subjob_cost_summary As scs
