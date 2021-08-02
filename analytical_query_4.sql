53e2263de647305026a081b1b2a24ff5b4f1e41a
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2021-08-02 09:38:25 -0700

    Update analytical_query_4.sql

--- analytical_query_4.sql
+++ analytical_query_4.sql
@@ -1,5 +1,5 @@
 Select irs.location_name, irs.time_year,
-	Sum(sum_invoice_amt -  sum_total_cost)/Sum(sum_invoice_amt) As profit_margin,
+	round(Sum(sum_invoice_amt -  sum_total_cost)/Sum(sum_invoice_amt)::numeric,4) As profit_margin,
 	RANK()OVER (PARTITION BY irs.time_year 
 		ORDER BY (Sum(sum_invoice_amt - sum_total_cost)/(Sum(sum_invoice_amt)))DESC) AS profit_margin_Rank
 From invoice_revenue_summary As irs, location_subjob_cost_summary As scs
