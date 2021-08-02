e5012aa09b3338c9080b6fd40e3ac7575bace51a
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2021-08-01 19:56:12 -0700

    Update base_query_4.sql

--- base_query_4.sql
+++ base_query_4.sql
@@ -1,11 +1,14 @@
-Select i.Location_id, Location_name, i.sales_class_id, sales_class_desc, t.time_year, t.time_month,
-	Sum(quantity_shipped - invoice_quantity) As Sum_qty_returned,
-	(Sum(invoice_amount)/Sum(invoice_quantity)) * Sum(quantity_shipped - invoice_quantity) As Sum_dollar_returned
-From w_invoiceline_f As i, w_location_d As l, w_sales_class_d As s, w_time_d As t
-Where i.location_id = l.location_id
-	And i.sales_class_id = s.sales_class_id
-	And i.invoice_sent_date = t.time_id
-	And quantity_shipped > invoice_quantity
-Group By i.Location_id, Location_name, i.sales_class_id, sales_class_desc, t.time_year, t.time_month
-Order By time_year, time_month
+SELECT i.location_id, l.location_name, i.sales_class_id, s.sales_class_desc, su.machine_type_id, m.machine_model, t.time_year, t.time_month,
+    Sum(i.quantity_shipped - i.invoice_quantity) AS sum_qty_returned,
+    Sum(i.invoice_amount) / sum(i.invoice_quantity)::numeric * sum(i.quantity_shipped - i.invoice_quantity)::numeric AS sum_dollar_returned
+FROM w_invoiceline_f i, w_location_d l, w_sales_class_d s, w_time_d t, w_sub_job_f su, w_machine_type_d m, w_job_shipment_f sh
+WHERE i.location_id = l.location_id 
+	AND i.sales_class_id = s.sales_class_id 
+	AND i.invoice_sent_date = t.time_id
+	AND su.sub_job_id = sh.sub_job_id
+	AND i.invoice_id = sh.invoice_id
+	AND su.machine_type_id = m.machine_type_id
+	AND i.quantity_shipped > i.invoice_quantity
+GROUP BY i.location_id, l.location_name, i.sales_class_id, s.sales_class_desc, su.machine_type_id, m.machine_model, t.time_year, t.time_month
+ORDER BY t.time_year, t.time_month;
 
