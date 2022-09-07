ef1564fb25d3aa522fc6ba589c3fd88fd7bd3d93
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2022-09-06 20:55:25 -0600

    Delete analytical_query_3.sql

--- analytical_query_3.sql
+++
@@ -1,7 +0,0 @@
-Select irs.location_name, irs.time_year,
-	Sum(sum_invoice_amt - sum_total_cost)::numeric(10,2) As profit,
-	RANK()OVER (PARTITION BY irs.time_year 
-		ORDER BY (Sum(sum_invoice_amt) - Sum(sum_total_cost))DESC) AS profit_Rank
-From invoice_revenue_summary As irs, location_subjob_cost_summary As scs
-Where irs.job_id = scs.job_id
-Group By irs.location_name, irs.time_year
