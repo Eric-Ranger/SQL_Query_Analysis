537ac990ee041d72dc363efd57932af9601c1064
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2022-09-06 20:54:30 -0600

    Delete analytical_query_1.sql

--- analytical_query_1.sql
+++
@@ -1,8 +0,0 @@
-Select Location_name, t.time_year, t.time_month, 
-	Sum(unit_price * quantity_ordered) As Sum_job_amount,
-	SUM(Sum(unit_price * quantity_ordered)) OVER(Partition By location_name, time_year 
-	ORDER BY t.time_month ROWS UNBOUNDED PRECEDING)::numeric(10,0) AS CumSum_amount_ordered
-From w_job_f As j, w_location_d As l, w_time_d As t
-Where j.location_id = l.location_id
-	And j.contract_date = t.time_id
-Group By Location_name, t.time_year, t.time_month
