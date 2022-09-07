58008b7b0c24305b6bdb41553511a1950bb210db
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2022-09-06 20:55:32 -0600

    Delete analytical_query_4.sql

--- analytical_query_4.sql
+++
@@ -1,7 +0,0 @@
-Select irs.location_name, irs.time_year,
-	round(Sum(sum_invoice_amt -  sum_total_cost)/Sum(sum_invoice_amt)::numeric,4) As profit_margin,
-	RANK()OVER (PARTITION BY irs.time_year 
-		ORDER BY (Sum(sum_invoice_amt - sum_total_cost)/(Sum(sum_invoice_amt)))DESC) AS profit_margin_Rank
-From invoice_revenue_summary As irs, location_subjob_cost_summary As scs
-Where irs.job_id = scs.job_id
-Group By irs.location_name, irs.time_year
