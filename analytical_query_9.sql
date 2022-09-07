6abe0f12555908615fbb902279c384cea95e164d
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2022-09-06 20:59:32 -0600

    Delete analytical_query_9.sql

--- analytical_query_9.sql
+++
@@ -1,7 +0,0 @@
-Select location_name, t.time_year As year_date_shipped, 
-	Sum(diff_bus_days) As sum_date_diff,
-	Rank() Over(Partition By time_year Order By Sum(diff_bus_days) Desc),
-		Dense_Rank() Over(Partition By time_year Order By Sum(diff_bus_days) Desc)
-From first_shipment_delays_shipping As f, w_time_d As t
-Where f.date_ship_by = t.time_id
-Group By location_name, t.time_year
