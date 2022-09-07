860e369e9ffbb73d1b3b360061a9adfd34faa65e
Eric-Ranger <76181977+Eric-Ranger@users.noreply.github.com>
2022-09-06 20:55:08 -0600

    Delete analytical_query_12.sql

--- analytical_query_12.sql
+++
@@ -1,7 +0,0 @@
-Select location_name, time_year,
-	Sum(days_to_prod) As sum_days_prod,
-	Rank() Over(Partition By time_year 
-		Order By Sum(days_to_prod)) As Rank_days_prod
-From delay_till_prod As d, w_time_d As t
-Where d.contract_date = t.time_id
-Group By location_name, time_year
